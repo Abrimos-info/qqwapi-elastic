b6da19ac1fb3ae642d53d3e5fbcc5c550d5bd32e
Martín Szyszlican <martin@abrimos.info>
2026-01-14 18:11:23 -0600

    Disable CORS headers (duplicated with nginx)

--- app.js
+++ app.js
@@ -1,95 +1,100 @@
-const express = require('express');
-const exegesisExpress = require('exegesis-express');
-const http = require('http');
-const path = require('path');
-const exegesisSwaggerUIPlugin = require( 'exegesis-plugin-swagger-ui-express' );
-const pjson = require('./package.json');
-const morgan = require('morgan');
-
+const express = require("express");
+const exegesisExpress = require("exegesis-express");
+const http = require("http");
+const path = require("path");
+const exegesisSwaggerUIPlugin = require("exegesis-plugin-swagger-ui-express");
+const pjson = require("./package.json");
+const morgan = require("morgan");
 
 async function createServer() {
-    const app = express();
+  const app = express();
 
-    //Log requests to console
-    app.use(morgan('short', {
-        // log only 4xx and 5xx responses to console
-        // skip: function (req, res) { return (res.statusCode < 400 && (req.headers.accept && req.headers.accept.indexOf("html") == -1 )) }
-    }))
-    
+  //Log requests to console
+  app.use(
+    morgan("short", {
+      // log only 4xx and 5xx responses to console
+      // skip: function (req, res) { return (res.statusCode < 400 && (req.headers.accept && req.headers.accept.indexOf("html") == -1 )) }
+    }),
+  );
 
-    app.use((request, response, next) => {
-        response.header('Access-Control-Allow-Origin', '*');
-        response.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
-        response.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  app.use((request, response, next) => {
+    // CORS headers are set by nginx to avoid duplication
+    // response.header('Access-Control-Allow-Origin', '*');
+    // response.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+    // response.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
 
-        //TODO: Don't cache on debug
-        response.header('Cache-Control', 'public; max-age: 700');
+    //TODO: Don't cache on debug
+    response.header("Cache-Control", "public; max-age: 700");
 
-        return next();
+    return next();
+  });
+
+  // Serve robots.txt that blocks all indexing
+  app.get("/robots.txt", (req, res) => {
+    res.type("text/plain");
+    res.send("User-agent: *\nDisallow: /");
+  });
+
+  // See https://github.com/exegesis-js/exegesis/blob/master/docs/Options.md
+  const options = {
+    controllers: path.resolve(__dirname, "./controllers"),
+    allErrors: true,
+    plugins: [
+      exegesisSwaggerUIPlugin({
+        // Express app (required)
+        app: app,
+        path: "/v3/docs",
+        // Options to pass to Swagger UI
+        swaggerUIOptions: {
+          explorer: true,
+        },
+      }),
+    ],
+  };
+
+  const exegesisMiddleware = await exegesisExpress.middleware(
+    path.resolve(__dirname, "./swagger.json"),
+    options,
+  );
+
+  // If you have any body parsers, this should go before them.
+  app.use(exegesisMiddleware);
+
+  app.use((req, res) => {
+    res.status(404).json({
+      es: `Aquí no hay nada. Por favor revisa la documentación: https://qqwapi-elastic.readthedocs.io/es/latest/`,
+      en: `There's nothing here. Please check the docs: https://qqwapi-elastic.readthedocs.io/es/latest/ (only in spanish)`,
+      version: pjson.version,
+      generated: new Date(),
     });
+  });
 
-    // Serve robots.txt that blocks all indexing
-    app.get('/robots.txt', (req, res) => {
-        res.type('text/plain');
-        res.send('User-agent: *\nDisallow: /');
-    });
-
-    // See https://github.com/exegesis-js/exegesis/blob/master/docs/Options.md
-    const options = {
-        controllers: path.resolve(__dirname, './controllers'),
-        allErrors: true,
-        plugins: [
-            exegesisSwaggerUIPlugin({
-                // Express app (required)
-                app: app,     
-                path:"/v3/docs",
-                // Options to pass to Swagger UI
-                swaggerUIOptions: {
-                    explorer: true
-                }
-            })
-        ]    
-    };
-
-    const exegesisMiddleware = await exegesisExpress.middleware(
-        path.resolve(__dirname, './swagger.json'),
-        options
+  app.use((err, req, res, next) => {
+    console.error(
+      "Internal error",
+      req.route.path,
+      err.meta ? err.meta.meta.request.params : "",
+      err.meta ? (err.meta.body ? err.meta.body.error : "") : "",
+      err,
     );
+    res.status(500).json({ "Internal error": err.message });
 
+    //If connection is lost to database, kill API process
+    if (err.name == "ConnectionError") {
+      process.exit(-1);
+    }
+  });
 
-    // If you have any body parsers, this should go before them.
-    app.use(exegesisMiddleware);
+  const server = http.createServer(app);
 
-    app.use((req, res) => {
-        res.status(404).json({
-            es: `Aquí no hay nada. Por favor revisa la documentación: https://qqwapi-elastic.readthedocs.io/es/latest/`,
-            en: `There's nothing here. Please check the docs: https://qqwapi-elastic.readthedocs.io/es/latest/ (only in spanish)`,
-            version: pjson.version,
-            generated: new Date()
-        });
-    });
+  const port = process.env.PORT || 10010;
 
-    app.use((err, req, res, next) => {
-        console.error("Internal error",req.route.path, err.meta ? err.meta.meta.request.params :"", err.meta ? err.meta.body ? err.meta.body.error : "" : "", err);
-        res.status(500).json({"Internal error": err.message});
-
-        //If connection is lost to database, kill API process
-        if (err.name == 'ConnectionError') {
-            process.exit(-1);
-        }
-    });
-
-      
-
-    const server = http.createServer(app);
-
-    
-    const port = process.env.PORT || 10010;
-
-    const l = server.listen(port, () => {
-        process.stdout.write(`Listening on http://localhost:${l.address().port}/\n`);
-    });
+  const l = server.listen(port, () => {
+    process.stdout.write(
+      `Listening on http://localhost:${l.address().port}/\n`,
+    );
+  });
 }
 
-console.log("QQWAPI",pjson.version);
+console.log("QQWAPI", pjson.version);
 createServer();
